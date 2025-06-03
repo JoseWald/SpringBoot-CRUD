7c7f1adc133e161841df64b92020402a5fbd1a4f
Votre Nouveau Nom <nouvel.email@exemple.com>
2025-06-03 12:17:04 +0300

    second commit

---
+++ src/main/java/Licence/diagnostic/DiagnosticApplication.java
@@ -0,0 +1,20 @@
+package Licence.diagnostic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
+public class DiagnosticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagnosticApplication.class, args);
+    }
+
+}
---
+++ src/main/java/Licence/diagnostic/HelloController.java
@@ -0,0 +1,13 @@
+package Licence.diagnostic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/helloworld")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
