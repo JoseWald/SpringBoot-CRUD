6cfe81f6b023f4d22be3aa9d7d3c1d2effb92135
unknown <rajaonarivonyjosewald@gmail.com>
2025-06-07 12:03:14 +0300

    error while deleting the licence

---
+++ src/main/java/com/userHandler/entities/User.java
@@ -0,0 +1,23 @@
+package com.userhandler.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;  // ADMIN ou USER
+
+    // Getters & Setters
+}
