071b972cfd29617b35b6d479d8c2c38c9b032b8a
unknown <rajaonarivonyjosewald@gmail.com>
2025-06-07 22:08:47 +0300

    connecting to pg admin && creating the table

---
+++ src/main/java/com/userhandler/entities/User.java
@@ -0,0 +1,37 @@
+package com.userhandler.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import  jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import  lombok.Getter;
+import  lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name="name",nullable=false)
+    String name;
+
+    @Column(name="password",nullable=false)
+    String password;
+
+    @Column(name="role")
+    Boolean isAdmin=false;//1:admin ; 0:employee
+
+    @Column(name="photo_path")
+    String photoPath;
+
+}
